eec0ed2983e5c002e0d02c469abbb1051175d5f1
Dave <dlp140@gmail.com>
2022-07-27 23:21:59 -0400

    wip - total scores accumulates

--- script.js
+++ script.js
@@ -1,1 +1,64 @@
-'use strict';
+'use strict'
+
+// Selecting score elements
+const player0El = document.querySelector('.player--0')
+const player1El = document.querySelector('.player--1')
+const score0El = document.querySelector('#score--0')
+const score1El = document.getElementById('score--1')
+const current0El = document.getElementById('current--0')
+const current1El = document.getElementById('current--1')
+
+const diceEl = document.querySelector('.dice')
+const btnNew = document.querySelector('.btn--new')
+const btnRoll = document.querySelector('.btn--roll')
+const btnHold = document.querySelector('.btn--hold')
+
+score0El.textContent = 0
+score1El.textContent = 0
+diceEl.classList.add('hidden')
+
+const scores = [0, 0]
+let currentScore = 0
+let activePlayer = 0
+
+const switchPlayer = () => {
+  document.getElementById(`current--${activePlayer}`).textContent = 0
+  currentScore = 0
+  activePlayer = activePlayer === 0 ? 1 : 0
+  player0El.classList.toggle('player--active')
+  player1El.classList.toggle('player--active')
+}
+
+// Rolling dice functionality
+btnRoll.addEventListener('click', () => {
+  // Generate random dice roll
+  const dice = Math.trunc(Math.random() * 6) + 1
+  console.log(dice)
+  // Display dice
+  diceEl.classList.remove('hidden')
+  diceEl.src = `dice-${dice}.png`
+  // Check for rolled 1: if true, next player
+  if (dice !== 1) {
+    // Add dice to current score
+    currentScore += dice
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore
+    // TODO -CHANGE LATER
+  } else {
+    // Switch to next player
+    switchPlayer()
+  }
+})
+
+btnHold.addEventListener('click', () => {
+  // Add current score to active player's total score and display it
+  scores[activePlayer] += currentScore
+
+  document.getElementById(`score--${activePlayer}`).textContent =
+    scores[activePlayer]
+
+  // Check if total score is >= 100 -> Finish game
+
+  // Switch players
+  switchPlayer()
+})
